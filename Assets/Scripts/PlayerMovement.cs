0d712e96bf3e5428459fc9c47c123bec4e45f97e
emstrubl <63114742+emstrubl@users.noreply.github.com>
2020-04-15 12:56:12 -0700

    Added distance from end using dot product

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,25 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float turnSpeed = 20f;
+    public float turnSpeed = 20.0f;
+    public GameObject gameEnding;
+    public Text distText;
+
 
     Animator m_Animator;
     Rigidbody m_Rigidbody;
-    AudioSource m_AudioSource;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
 
+    // Start is called before the first frame update
     void Start()
     {
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
-        m_AudioSource = GetComponent<AudioSource>();
     }
 
-    void FixedUpdate()
+    // Update is called once per frame
+    void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
@@ -32,25 +36,25 @@ public class PlayerMovement : MonoBehaviour
         bool isWalking = hasHorizontalInput || hasVerticalInput;
         m_Animator.SetBool("IsWalking", isWalking);
 
-        if (isWalking)
-        {
-            if (!m_AudioSource.isPlaying)
-            {
-                m_AudioSource.Play();
-            }
-        }
-        else
-        {
-            m_AudioSource.Stop();
-        }
-
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
+
+        SetDistanceText();
     }
 
     void OnAnimatorMove()
     {
         m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
         m_Rigidbody.MoveRotation(m_Rotation);
+    }
+
+    void SetDistanceText()
+    {
+        Vector3 endPos = gameEnding.transform.position;
+        //vector between the player and the end
+        Vector3 goalVector = endPos - transform.position;
+        float distance = Mathf.Sqrt(Vector3.Dot(goalVector, goalVector));
+        distText.text = Mathf.Round(distance) + " units from the end!";
+
     }
 }
